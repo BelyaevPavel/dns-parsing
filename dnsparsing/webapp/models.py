2dbe38e19d9d000f3aa42f2913c14bfced5d4d2d
Palov <gbelbih@gmail.com>
2016-08-01 18:02:52 +0500

    Type of product_price field changed from char to float

--- dnsparsing/webapp/models.py
+++ dnsparsing/webapp/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length = 100)
     hreference = models.CharField(max_length = 400)
-    product_price = models.CharField(max_length = 7)
+    product_price = models.FloatField()
     def __str__(self):
         return '%s' % (self.product_name)
