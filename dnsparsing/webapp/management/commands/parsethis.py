b5a1dd54c170e52a540e9b38f70ce28d497e792b
Palov <gbelbih@gmail.com>
2016-08-09 02:35:29 +0500

    parsethis command was modified to take city id's from data base

--- dnsparsing/webapp/management/commands/parsethis.py
+++ dnsparsing/webapp/management/commands/parsethis.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 import requests
+from django.db.models import Q
 from django.core.management.base import BaseCommand, CommandError
 from lxml import html
 
@@ -27,19 +29,22 @@ class Command(BaseCommand):
                 sess.headers.update(headers)
                 sess.get('http://www.dns-shop.ru/')
                 cookies_dict = requests.utils.dict_from_cookiejar(sess.cookies)
-                city_list=City.objects.all()
-                for k in range(len(city_list)):
-                    cookies_dict['city_guid_1c'] = city_list[k].city_id
+                city_list = City.objects.filter(
+                    Q(city_name__exact='Челябинск') | Q(city_name__exact='Магнитогорск') | Q(
+                        city_name__exact='Екатеринбург') | Q(city_name__exact='Пермь') | Q(
+                        city_name__exact='Сургут') | Q(
+                        city_name__exact='Тюмень') | Q(city_name__exact='Курган'))
+                for city in city_list:
+                    cookies_dict['city_guid_1c'] = city.city_id
                     page = sess.get(url=url, cookies=cookies_dict)
                     root = html.fromstring(page.content)
-                    product_name_list = [e.text for e in
-                                         root.cssselect("div.product div.item-name a.ec-price-item-link")]
-                    hreference_list = ['www.dns-shop.ru' + e.get('href') for e in
-                                       root.cssselect("div.product div.item-name a.ec-price-item-link")]
+                    aux_names_and_refs = root.cssselect("div.product div.item-name a.ec-price-item-link")
+                    product_name_list = [e.text for e in aux_names_and_refs]
+                    hreference_list = ['www.dns-shop.ru' + e.get('href') for e in aux_names_and_refs]
                     product_price_list = [e.text for e in
                                           root.cssselect(
                                               "div.product div.item-price span[data-product-param=\"price\"]")]
-                    self.stdout.write(city_list[k].city_name)
+                    self.stdout.write(city.city_name)
                     for i in range(len(product_name_list)):
                         p = Product(product_name=product_name_list[i], hreference=hreference_list[i],
                                     product_price=product_price_list[i].replace(' ', ''))
@@ -48,6 +53,6 @@ class Command(BaseCommand):
                             self.style.SUCCESS(' %s %s %s' % (p.product_name, p.product_price, p.hreference)))
                     self.stdout.write(self.style.SUCCESS('Successfully filled'))
             except Exception:
-                raise CommandError('Something goes wrong in %s' % url)
+                raise CommandError('Something goes wrong while processing %s.' % url)
 
             self.stdout.write(self.style.SUCCESS('Successfully parsed url "%s"' % url))
