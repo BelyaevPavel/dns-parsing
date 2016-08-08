bce50cebd614a721e72d87d684e50d88f2e02b05
Palov <gbelbih@gmail.com>
2016-08-08 19:31:28 +0500

    Update option added

--- dnsparsing/webapp/management/commands/getcityids.py
+++ dnsparsing/webapp/management/commands/getcityids.py
@@ -1,5 +1,6 @@
 import requests
 from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from lxml import html
 
 from webapp.models import City
@@ -13,7 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            City.objects.all().delete()
+            # City.objects.all().delete()
             headers = {
                 'Host': 'www.dns-shop.ru',
                 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0',
@@ -25,13 +26,28 @@ class Command(BaseCommand):
             sess = requests.Session()
             sess.headers.update(headers)
             page = sess.get('http://www.dns-shop.ru/ajax/region-nav-window/')
-            root = html.fromstring(page.content)
+            root = html.fromstring(page.content.decode('utf-8'))
             city_id_list = [e.get('data-city-id') for e in root.cssselect("ul.cities a")]
             city_name_list = [e.text for e in root.cssselect("ul.cities a")]
             for i in range(len(city_name_list)):
-                c = City(city_name=city_name_list[i], city_id=city_id_list[i])
-                c.save()
-                self.stdout.write(self.style.SUCCESS('%s %s %s' % (c.city_id, c.city_name, c.id)))
+                try:
+                    c = City.objects.get(city_name=city_name_list[i])
+                    if c.city_id != city_id_list[i]:
+                        city_id_old = c.city_id
+                        c.city_id = city_id_list[i]
+                        c.save()
+                        self.stdout.write(
+                            self.style.SUCCESS('UPDATED: %s %s to %s' % (c.city_name, city_id_old, c.city_id)))
+                    else:
+                        self.stdout.write('ALREADY UP TO DATE: %s %s' % (c.city_name, c.id))
+                except ObjectDoesNotExist:
+                    c = City(city_name=city_name_list[i], city_id=city_id_list[i])
+                    c.save()
+                    self.stdout.write(self.style.SUCCESS('ADDED: %s %s %s' % (c.city_name, c.city_id, c.id)))
+                except MultipleObjectsReturned:
+                    self.stdout.write(
+                        self.style.ERROR('Multiple objects were found while searching %s' % city_name_list[i]))
+
             self.stdout.write(self.style.SUCCESS('Successfully filled'))
         except Exception:
             raise CommandError('Something goes wrong')
