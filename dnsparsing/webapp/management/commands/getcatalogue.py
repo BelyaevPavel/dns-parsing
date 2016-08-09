c8558263e65d64d9fbd74c07326ad739e5aa63df
Palov <gbelbih@gmail.com>
2016-08-09 20:36:56 +0500

    getcatalogue command full functional

--- dnsparsing/webapp/management/commands/getcatalogue.py
+++ dnsparsing/webapp/management/commands/getcatalogue.py
@@ -27,18 +27,17 @@ class Command(BaseCommand):
             page = sess.get(base_url)
             root = html.fromstring(page.content.decode('utf-8'))
             top_level_refs_list = [base_url + e.get('href') for e in root.cssselect("ul.catalog a.catalog-icon")]
-            # for top_level_ref in top_level_refs_list:
-            catalogue_refs_list.extend(
-                get_next_level_refs('http://www.dns-shop.ru/catalog/17aa731316404e77/vstraivaemaya-texnika/',
-                                    'top-level', sess))
+            for top_level_ref in top_level_refs_list:
+                catalogue_refs_list.extend(
+                    get_next_level_refs(top_level_ref, 'top-level', sess))
             self.stdout.write(self.style.SUCCESS('catalogue_refs_list filled %s' % len(catalogue_refs_list)))
             for catalogue_ref in catalogue_refs_list:
-                try:
-                    self.stdout.write(
-                        self.style.SUCCESS('entered a cycle %s %s' % catalogue_ref[0], catalogue_ref[1]))
-                    c = Category.objects.get(category_name=catalogue_ref[1])
-                    self.stdout.write(
-                        self.style.SUCCESS('c found'))
+                self.stdout.write(
+                    self.style.SUCCESS('entered a cycle %s %s' % (catalogue_ref[0], catalogue_ref[1])))
+                # c = Category.objects.get(category_name=catalogue_ref[1])
+                c, created = Category.objects.get_or_create(category_name=catalogue_ref[1])
+                if not created:
+                    self.stdout.write(self.style.SUCCESS('c found'))
                     if c.category_reference != catalogue_ref[0]:
                         category_reference_old = c.category_reference
                         c.category_reference = catalogue_ref[0]
@@ -46,21 +45,18 @@ class Command(BaseCommand):
                             self.style.SUCCESS('category parameters set'))
                         c.save()
                         self.stdout.write(
-                            self.style.SUCCESS('UPDATED: %s %s to %s' % (
+                            self.style.SUCCESS('UPDATED: %s %s -> %s' % (
                                 c.category_name, category_reference_old, c.category_reference)))
                     else:
                         self.stdout.write('ALREADY UP TO DATE: %s %s' % (c.id, c.category_name))
-                except ObjectDoesNotExist:
-                    c = Category(city_name=catalogue_ref[1], city_id=catalogue_ref[0])
+                else:
+                    c.category_reference = catalogue_ref[0]
+                    c.category_name = catalogue_ref[1]
                     c.save()
                     self.stdout.write(
                         self.style.SUCCESS('ADDED: %s %s %s' % (c.id, c.category_name, c.category_reference)))
-                except MultipleObjectsReturned:
-                    self.stdout.write(
-                        self.style.ERROR('Multiple objects were found while searching %s' % catalogue_ref[1]))
-
             self.stdout.write(self.style.SUCCESS('Successfully filled'))
         except Exception:
-            raise CommandError('Something goes wrong')
+            raise CommandError('Something bad happen %s' % Exception.args)
 
         self.stdout.write(self.style.SUCCESS('Successfully parsed'))
